b62893c5f0f44748bfa03b0a7359bb0affbeb633
gaangsarr <anjasgangsar12@gmail.com>
2025-10-22 10:11:24 +0700

    tutup stat

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
       <Sambutan />
       <ScrollingText />
       <Tiket />
-      <RegistrationStats />
+      {/* <RegistrationStats /> */}
       <ScrollingText1 />
       <GamePromo />
       <FAQ />
